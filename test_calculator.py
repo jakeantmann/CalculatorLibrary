4a90597a9962ea154f6d589232b4e1398ee1db74
Jake Antmann <jakeantmann@gmail.com>
2021-08-03 10:53:09 +1000

    Remove venv

---
+++ test_calculator.py
@@ -0,0 +1,14 @@
+"""Unit tests for the calculator library."""
+
+import calculator
+
+
+class TestCalculator:
+    """Test the calculator."""
+    def test_addition(self):
+        """Test addition."""
+        assert 4 == calculator.add(2, 2)
+
+    def test_subtraction(self):
+        ""Test subtraction."""
+        assert 2 == calculator.subtract(4, 2)
